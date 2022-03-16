7adb063545060fd349990b58d71befe3586bdebf
Rainer <rainermr89@hotmail.com>
2022-03-16 13:04:11 -0300

    adicionando service

---
+++ CM.Service/Imp/MovieService.cs
@@ -0,0 +1,49 @@
+﻿using CM.Data.Interfaces;
+using CM.Domain;
+using CM.Service.Interface;
+
+namespace CM.Service.Imp
+{
+    public class MovieService : IMovieService
+    {
+        private readonly IMovieRepository _repository;
+
+        public MovieService(IMovieRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Add(Movie movie)
+        {
+            if(movie != null)
+            {
+                _repository.Add(movie);
+            }
+            else
+            {
+                Console.WriteLine("Campo requerido");
+            }
+            
+        }
+
+        public void Delete(Guid id)
+        {
+            _repository.Delete(id);
+        }
+
+        public Movie Get(Guid id)
+        {
+            return _repository.Get(id);
+        }
+
+        public IEnumerable<Movie> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public void Update(Guid id, Movie movie)
+        {
+            _repository.Update(id, movie);
+        }
+    }
+}
---
+++ CM.Service/Interface/IMovieService.cs
@@ -0,0 +1,13 @@
+﻿using CM.Domain;
+
+namespace CM.Service.Interface
+{
+    public interface IMovieService
+    {
+        void Add(Movie movie);
+        IEnumerable<Movie> GetAll();
+        Movie Get(Guid id);
+        void Update(Guid id, Movie movie);
+        void Delete(Guid id);
+    }
+}
